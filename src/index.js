c1fb1c923bec3dbc1b9d9532929b1460a83095e9
keep_chr <dobrozhan.a@gmail.com>
2020-03-05 02:21:28 +0200

    feat: morse-decoder task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,38 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+
+    var charArr = expr.match(/.{1,10}/g);
+    var char = [];
+    var msg = [];
+    
+
+    for (let i = 0; i < charArr.length; i++) {
+        if (charArr[i] === '**********') {
+            char.push(' ');
+            msg.push(' ');
+        } else {
+            chars = charArr[i].match(/.{1,2}/g);
+            let str = '';
+            chars.forEach(item => {
+                switch (item) {
+                    case '10':
+                        str += '.';
+                        break;
+                    case '11':
+                        str += '-';
+                        break;
+                    default:
+                        break;
+                }
+            });
+            char.push(str);
+
+            msg.push(MORSE_TABLE[char[i]]);
+        }
+    }
+
+    return msg.join('');
 }
 
 module.exports = {
